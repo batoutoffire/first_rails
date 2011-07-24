691bb9c8fe1f7dbd71bc3a75a792aa2ba1bca079
Amro <amr.abdulhakim@gmail.com>
2011-07-24 13:53:56 +0530

    Initial cpmmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'f1269a3cfa028d1abae2ac513ad2b92b7625bc8fdef82e3b0fe4a007c8ceded1fc18d22020e21ad6445beb462a184a923709eb06eb1f1ecc6a864f3945d4f186'
